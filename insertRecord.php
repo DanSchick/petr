85f8395c62084a713db35f63e4827d8bacb413fd
Danny Schick <dschick@uvm.edu>
2015-11-21 05:53:52 -0500

    got match working in terms of SQL statements

--- insertRecord.php
+++ insertRecord.php
@@ -4,34 +4,73 @@ if($_POST){
     $userID = $_POST['userid'];
     $profileID = $_POST['profileid'];
     $liked = $_POST['like'];
+    echo $userID;
+    echo $profileID;
+    echo $liked;
     $q = 'SELECT fldLiked FROM tblRelations WHERE fnkUserId = ? AND fnkProfileId = ?';
     $data = array($profileID, $userID);
-    $match = $thisDatabaseReader->select($query, $data, 1, 1);
-    if($liked == 'T' AND $match[0]['fldLiked'] == 'T'){
-        echo 'Match';
-        $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
-        $data = array($userID, $profileID, $liked, 'T');
-        $insert = $thisDatabaseWriter->insert($query, $data);
+    $match = $thisDatabaseReader->select($q, $data, 1, 1);
+    if($match[0]['fldLiked'] == ""){
+        if($liked == 'T'){
+            echo 'flag';
+            echo 'no match but likedI';
+            $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
+            $data = array($userID, $profileID, $liked, 'F');
+            $insert = $thisDatabaseWriter->insert($query, $data);
+        } else if($liked == 'F'){
+            echo 'no match and dislikedIE';
+            $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
+            $data = array($userID, $profileID, $liked, 'F');
+            $insert = $thisDatabaseWriter->insert($query, $data);
+        }
+    } else if ($match[0]['fldLiked'] == 'T'){
+        if($liked == 'T'){
+            echo 'MatchII';
+            $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
+            $data = array($userID, $profileID, $liked, 'T');
+            $insert = $thisDatabaseWriter->insert($query, $data);
 
-        $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
-        $data = array($profileID, $userID, $liked, 'T');
-        $insert = $thisDatabaseWriter->insert($query, $data);
-    } else{
-        echo 'no match';
-        $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
-        $data = array($userID, $profileID, $liked, 'F');
-        $insert = $thisDatabaseWriter->insert($query, $data);
+            $query = 'UPDATE DSCHICK_Pettr.tblRelations SET fldMatched=? WHERE fnkUserId = ? AND fnkProfileId = ? ';
+            $data = array('T', $profileID, $userID);
+            $insert = $thisDatabaseWriter->insert($query, $data, 1, 1);
+        } else if ($liked == 'F'){
+            echo 'Would have been match but dislikedIIE';
+            $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
+            $data = array($userID, $profileID, $liked, 'F');
+            $insert = $thisDatabaseWriter->insert($query, $data);
+        }
     }
+    // echo $match[0]['fldLiked'];
+
+    // if($liked == 'T' AND $match[0]['fldLiked'] == 'T'){
+    //     echo 'Match';
+    //     $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
+    //     $data = array($userID, $profileID, $liked, 'T');
+    //     $insert = $thisDatabaseWriter->insert($query, $data);
+
+    //     $query = 'UPDATE DSCHICK_Pettr.tblRelations SET fldMatched=? WHERE fnkUserId = ? AND fnkProfileId = ? ';
+    //     $data = array('T', $profileID, $userID);
+    //     $insert = $thisDatabaseWriter->insert($query, $data);
+    // } else {
+        // echo 'no match but liked';
+        // $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
+        // $data = array($userID, $profileID, $liked, 'F');
+        // $insert = $thisDatabaseWriter->insert($query, $data);
+
+    //  }//else{
+    //     echo 'no match and disliked';
+    //     $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkUserId, fnkProfileId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
+    //     $data = array($userID, $profileID, $liked, 'F');
+    //     $insert = $thisDatabaseWriter->insert($query, $data);
+    // }
     $query = 'UPDATE DSCHICK_Pettr.tblSeen SET fldSeen=? WHERE pmkUserId = ? AND fnkProfileId = ?';
-    $data = array(1, $userID, $profileID);
+    $data = array('1', $userID, $profileID);
     $update = $thisDatabaseWriter->update($query, $data, 1, 1);
-
-
 
     //echo 'okay sick';
 
 } else {
-    print '<p>no post</p>';
+    print '<p>natcasesort(array)o post</p>';
     // $query = 'INSERT INTO DSCHICK_Pettr.tblRelations (fnkNonOwnerId, fnkOwnerId, fldLiked, fldMatched) VALUES (?, ?, ?, ?)';
     // $data = array('dschick', 'atbarnes', 1, 0);
     // $insert = $thisDatabaseWriter->testquery($query, $data);
@@ -41,7 +80,7 @@ if($_POST){
     // $update = $thisDatabaseWriter->testquery($query, $data, 1, 0);
 
     $q = 'SELECT fldLiked FROM tblRelations WHERE fnkUserId = ? AND fnkProfileId = ?';
-    $data = array('atbarnes', 'dschick');
+    $data = array('spakulsk', 'dschick');
     $match = $thisDatabaseReader->select($q, $data, 1, 1);
     if($match[0]['fldLiked'] == 'T'){
         print '<p>we got match</p>';
