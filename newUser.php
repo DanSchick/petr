3e95a9cb3b3e7b4fef9fc2f6243c94e82d799392
Danny Schick <dschick@uvm.edu>
2015-12-09 20:51:18 -0500

    new user insert

--- newUser.php
+++ newUser.php
@@ -5,6 +5,10 @@ VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, ?)';
 $data = array($username);
 $insert = $thisDatabaseWriter->insert($q, $data, 0, 0);
 
+$q2 = "INSERT INTO `DSCHICK_Pettr`.`tblRelations` (`fnkUserId`, `fnkProfileId`, `fldLiked`, `fldMatched`) VALUES (?, 'e', 'T', 'F')";
+$data = array($username);
+$ins = $thisDatabaseWriter->testquery($q2, $data, 0, 0, 6);
+
 $query = 'SELECT fldURL FROM tblPhotos';
 $photo = $thisDatabaseReader->select($query, "", 0, 0, 0, 0);
 
@@ -15,10 +19,13 @@ shuffle($pics);
 <div id='holder'></div>
 <article id='card' class='box animate fadeInLeft one'>
     <section class="cardTitle">
-        <h1 class="petTitle">Welcome to Petr!</h1>
+        <h1 class="petTitle">Welcome to Petr!  </h1>
     </section>
     <div id='wrapper'>
-            <a href='profileUpdate.php' data-ajax='false'><h1>Click here to start editing your profile!</h1></a>
+            <h1>This site is used to mactch dog owners with other dog owners to bring their dogs together.<br>
+        You can upload photos of your dog and change information, and then you 'like' or 'pass' on other user's dogs.<br>
+        If the other user 'liked' your dog, you both 'match'! You can then view more information in the matches page.
+        <br><br><a href='profileUpdate.php' data-ajax='false'>Click here to start editing your profile!</h1></a>
     </div>
     <div class = "petImageHolder" id="container">
             <div class="buddy" style="display: inline-block;">
